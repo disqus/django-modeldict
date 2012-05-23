1935f6ba201624b6984dba36568ff84e82cd8bc1
David Cramer <dcramer@gmail.com>
2012-05-23 15:13:11 -0700

    1.3.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependency_links = [
 
 setup(
     name='django-modeldict',
-    version='1.3.0',
+    version='1.3.1',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/django-modeldict/',
