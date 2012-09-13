e6a9e32c80351a555b05a3bb4328d404986ff9e6
David Cramer <dcramer@gmail.com>
2012-09-13 13:10:28 -0700

    1.3.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = [
 
 setup(
     name='django-modeldict',
-    version='1.3.5',
+    version='1.3.6',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/django-modeldict/',
