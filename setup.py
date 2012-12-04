8031a713ec768616b44121755479313543ee73ca
David Cramer <dcramer@gmail.com>
2012-12-04 11:49:10 -0800

    1.4.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='django-modeldict',
-    version='1.4.0',
+    version='1.4.1',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/django-modeldict/',
